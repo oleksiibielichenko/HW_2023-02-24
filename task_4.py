dda07af631bb3f75026fdbea589ba87292f4641d
Oleksii Bielichenko <byelichenko@gmail.com>
2023-02-24 00:58:24 +0200

    task 4 done

---
+++ task_4.py
@@ -0,0 +1,19 @@
+num_1 = 15
+num_2 = 23
+num_3 = 13
+user_age = int(input("Please, enter your age: "))
+print("<15: ", user_age < num_1)
+print("<=15: ", user_age <= num_1)
+print(">15: ", user_age > num_1)
+print(">=15: ", user_age >= num_1)
+print("=15: ", user_age == num_1)
+print("<23: ", user_age < num_2)
+print("<=23: ", user_age <= num_2)
+print(">23: ", user_age > num_2)
+print(">=23: ", user_age >= num_2)
+print("=23: ", user_age == num_2)
+print("<13: ", user_age < num_3)
+print("<=13: ", user_age <= num_3)
+print(">13: ", user_age > num_3)
+print(">=13: ", user_age >= num_3)
+print("=13: ", user_age == num_3)
